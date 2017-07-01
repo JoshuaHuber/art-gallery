52b7dd394aa06e088a0d1405855340557efa7d41
fastexposure <fastexposure@yahoo.com>
2017-07-01 16:51:12 -0700

    added artist obj and delete btn

--- js/app.js
+++ js/app.js
@@ -9,8 +9,7 @@ const init = () => {
 
 function ArtistGallery(){
     const gallery = this;
-    this.artistFirstInput = $("#artistFirst");
-    this.artistLastInput = $("#artistLast");
+    this.artistNameInput = $("#artistName");
     this.dobInput = $("#dob");
     this.emailInput = $("#email");
     this.addBtn = $("#addBtn");
@@ -21,7 +20,7 @@ function ArtistGallery(){
         this.cancelBtn.click(this.clear);
     }
     this.handleAddClicked = () => {
-        const name = this.artistFirstInput.val() + " " + this.artistLastInput.val();
+        const name = this.artistNameInput.val();
         const artist = new AddArtist(this);
         this.artists[name] = artist;
         this.addArtistToDom(this.artists);
@@ -29,5 +28,42 @@ function ArtistGallery(){
     }
     this.clear = () => {
         console.log("cancel")
+    }
+    function AddArtist(gallery){
+        const artist = this;
+        this.name = gallery.artistNameInput.val();
+        this.dob = gallery.dobInput.val();
+        this.email = gallery.emailInput.val();
+    }
+    this.addArtistToDom = (obj) => {
+        this.removeAllArtistDomElements();
+        const $tableBody = $('#tbody');
+        for (let i in obj){
+            const $tr = $('<tr>');
+            const $name = $('<td>').text(obj[i].name);
+            const $dob = $('<td>').text(obj[i].dob);
+            const $email = $('<td>').text(obj[i].email);
+            const $button = $('<button>', {
+                type: 'button',
+                id: 'DeleteArtistBtn' + i,
+                class: 'btn btn-danger',
+                text: 'Delete'
+            });
+            var index = i;
+            $tableBody.append($tr.append($name, $dob, $email, $button));
+            this.handleIndividualDelete($button, obj[i].name);
+        }
+    }
+    this.removeAllArtistDomElements = () => {
+        const $tableBody = $('#tbody').empty();
+    }
+    this.removeArtistFromDom = (targetButton) => {
+        targetButton.parent($('<tr>')).remove();
+    }
+    this.handleIndividualDelete = (targetButton, name) => {
+        targetButton.click(function() {
+            delete gallery.artists[name];
+            gallery.removeArtistFromDom(targetButton);
+       })
     }
 }
